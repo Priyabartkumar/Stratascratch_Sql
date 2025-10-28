f415219913262b8603737ff05350165c323931b1
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-10-28 10:41:16 +0530

    Update Salaries_Difference.sql

--- Salaries_Difference.sql
+++ Salaries_Difference.sql
@@ -1,2 +1,4 @@
-Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.
+Q)Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.
+
+select(select max(salary)  from db_employee where department_id = 4)- (select max(salary)  from db_employee where department_id=1);
 
