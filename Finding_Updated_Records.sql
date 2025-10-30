77977acc6eb77950e301d30530bcfa4065f5ec04
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-10-30 07:33:07 +0530

    Add SQL query to find updated employee salaries
    Query to find current salaries of employees, ordered by ID.

--- Finding_Updated_Records.sql
+++ Finding_Updated_Records.sql
@@ -1,2 +1,6 @@
 We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
 
+select distinct id , first_name, last_name, department_id, 
+  max(salary) OVER (Partition by id) as salary 
+  from ms_employee_salary
+order by id;
