f5de2d42b5bfe8c68abad41573daab442d121ac3
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-01 07:47:06 +0530

    Update Bikes_Last_Used.sql

--- Bikes_Last_Used.sql
+++ Bikes_Last_Used.sql
@@ -1,2 +1,5 @@
-Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most recently used.
+// Q)Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most recently used.
 
+select bike_number, max(end_time) as last_used from dc_bikeshare_q1_2012
+group by bike_number, end_time
+order by end_time desc;
