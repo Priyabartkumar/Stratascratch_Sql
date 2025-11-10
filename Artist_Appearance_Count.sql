a08b8f11f4ad266e04b4d76aa0f565553bf1421b
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-11 00:09:18 +0530

    Update Artist_Appearance_Count.sql

--- Artist_Appearance_Count.sql
+++ Artist_Appearance_Count.sql
@@ -1,1 +1,3 @@
-
+Find how many times each artist appeared on the Spotify ranking list.
+Output the artist name along with the corresponding number of occurrences.
+Order records by the number of occurrences in descending order
