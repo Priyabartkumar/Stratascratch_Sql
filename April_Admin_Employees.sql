350574c73b945ebe7052c7e9954e11fbce327f5e
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-17 00:19:25 +0530

    Update April_Admin_Employees.sql

--- April_Admin_Employees.sql
+++ April_Admin_Employees.sql
@@ -6,4 +6,7 @@ where joining_date Between '2014-04-01' and curdate() and department = 'Admin';
 
          //OR
 
-  
+ SELECT 
+    COUNT(worker_id) 
+    FROM worker
+WHERE MONTH(joining_date) >=4 AND department = 'Admin'; 
