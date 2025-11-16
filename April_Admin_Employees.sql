61b504a2d7424a5dbf5f7fd4934fc91dca073284
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-17 00:18:52 +0530

    Update April_Admin_Employees.sql

--- April_Admin_Employees.sql
+++ April_Admin_Employees.sql
@@ -1,2 +1,9 @@
 Q)Find the number of employees working in the Admin department that joined in April or later, in any year.
+
+select count(worker_id)
+from worker
+where joining_date Between '2014-04-01' and curdate() and department = 'Admin';
+
+         /// OR
+
   
