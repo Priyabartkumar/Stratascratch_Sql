3e054b279b53f19a172e0cdf9576ae60675277a4
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-28 04:47:22 +0530

    Update SQL query to filter orders by customer names

--- Order_Details.sql
+++ Order_Details.sql
@@ -5,7 +5,7 @@ Consider the Jill and Eva as first names of customers.
 Output the order date, details and cost along with the first name.
 Order records based on the customer id in ascending order.
 
- 
+ Query:
 select first_name,order_date,order_details, total_order_cost
 from customers cs join orders o on cs.id = o.cust_id
 where first_name='Jill' or first_name = 'Eva'
