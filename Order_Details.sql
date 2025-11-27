4435f26e1554fd40995ede0751830a68b112836d
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-11-28 04:47:07 +0530

    Update Order_Details.sql

--- Order_Details.sql
+++ Order_Details.sql
@@ -4,3 +4,9 @@ Find order details made by Jill and Eva.
 Consider the Jill and Eva as first names of customers.
 Output the order date, details and cost along with the first name.
 Order records based on the customer id in ascending order.
+
+ 
+select first_name,order_date,order_details, total_order_cost
+from customers cs join orders o on cs.id = o.cust_id
+where first_name='Jill' or first_name = 'Eva'
+order by o.cust_id;
