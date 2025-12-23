afbd6542e3d8a381a363dc231427e301217159b6
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2025-12-23 23:39:37 +0530

    Add SQL query for identifying returning active users
    Query to identify returning active users who made a second purchase within 1 to 7 days after their first purchase, excluding same-day purchases.

--- Finding_User_Purchases.sql
+++ Finding_User_Purchases.sql
@@ -1,1 +1,12 @@
 Identify returning active users by finding users who made a second purchase within 1 to 7 days after their first purchase. Ignore same-day purchases. Output a list of these user_ids.
+
+
+  
+select distinct t1.user_id, min(t1.created_at) as first_purchase
+from amazon_transactions t1 join 
+amazon_transactions t2
+on t1.user_id=t2.user_id
+and t1.id<>t2.id
+where abs(datediff(first_purchase,t2.created_at))Between 1 and 7
+group by t1.user_id
+order by t1.user_id;
