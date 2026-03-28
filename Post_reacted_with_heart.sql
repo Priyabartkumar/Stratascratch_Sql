67f3bc669a72c9ec4de109c35a087a2d56b6bbdf
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-03-28 10:56:48 +0530

    Update Post_reacted_with_heart.sql

--- Post_reacted_with_heart.sql
+++ Post_reacted_with_heart.sql
@@ -1,1 +1,1 @@
-Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+Q)Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
