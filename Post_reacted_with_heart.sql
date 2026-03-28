84281e93303c320b5b72755a1b3471fa0f495864
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-03-28 10:57:30 +0530

    Query posts reacted with a heart from facebook_posts

--- Post_reacted_with_heart.sql
+++ Post_reacted_with_heart.sql
@@ -1,1 +1,3 @@
 Q)Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+select * from facebook_posts
+where post_id in (Select post_id from facebook_reactions where reaction = 'heart');
