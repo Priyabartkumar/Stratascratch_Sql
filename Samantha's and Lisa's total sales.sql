4d4279b16b15b726083efd655210afe9819b42af
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-03-31 23:19:11 +0530

    Add SQL query for total sales revenue of Samantha and Lisa

--- Samantha's and Lisa's total sales.sql
+++ Samantha's and Lisa's total sales.sql
@@ -1,4 +1,6 @@
 What is the total sales revenue of Samantha and Lisa?
 
 Solution:
-
+select sum(sales_revenue) as total_revenue from sales_performance where 
+salesperson = 'Samantha' or
+salesperson ='Lisa';
