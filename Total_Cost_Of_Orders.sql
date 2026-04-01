06ec2f951d8c34e10d4544f47731774ce6912dbe
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-01 09:49:58 +0530

    Fix comment formatting in SQL query

--- Total_Cost_Of_Orders.sql
+++ Total_Cost_Of_Orders.sql
@@ -1,1 +1,1 @@
-Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. Order records by customer's first name alphabetically.
+q) Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. Order records by customer's first name alphabetically.
