b08a05fd6a6096d651288d568aa9e63de9549db1
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-01 09:51:43 +0530

    Fix SQL query for total cost of customer orders

--- Total_Cost_Of_Orders.sql
+++ Total_Cost_Of_Orders.sql
@@ -1,7 +1,8 @@
 Q) Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. Order records by customer's first name alphabetically.
 
-select orders.cust_id,customers.first_name, sum(total_order_cost) from orders
+Solution:
+ select orders.cust_id,customers.first_name, sum(total_order_cost) from orders
  join customers on  customers.id = orders.cust_id
-group by customers.id,
+ group by customers.id,
          customers.first_name
-order by customers.first_name asc;
+ order by customers.first_name asc;
