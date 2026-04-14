12e79956d79f0689f43b958f7209e824deb019af
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-14 23:55:36 +0530

    Update Samantha's and Lisa's total sales.sql

--- Samantha's and Lisa's total sales.sql
+++ Samantha's and Lisa's total sales.sql
@@ -1,6 +1,7 @@
 Q)
   What is the total sales revenue of Samantha and Lisa?
 
+  Easy
 Solution:
 select sum(sales_revenue) as total_revenue from sales_performance where 
 salesperson = 'Samantha' or
