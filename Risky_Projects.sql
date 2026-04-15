48a191ca4179f2892e47f3aa91cfd0333b89cd92
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-15 11:32:07 +0530

    Update Risky_Projects.sql

--- Risky_Projects.sql
+++ Risky_Projects.sql
@@ -7,6 +7,7 @@ For example, if an employee works on a six-month project, only half of their ann
 Your output should be a list of overbudget projects, where each entry includes the project’s name, its budget, and the total prorated employee expenses for that project. 
 The total expenses should be rounded up to the nearest dollar. Assume all years have 365 days and disregard leap years.
 
+
 Queries:
 
 select a.title,a.budget,
