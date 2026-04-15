6f5bc5f2f4f69fa36ace912ad6e35a19184ca0fa
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-15 11:31:24 +0530

    Update Risky_Projects.sql

--- Risky_Projects.sql
+++ Risky_Projects.sql
@@ -6,3 +6,14 @@ A project is considered overbudget if the prorated cost of all employees assigne
 For example, if an employee works on a six-month project, only half of their annual salary should be attributed to that project. Sum these prorated salary amounts for all employees assigned to a project and compare the total with the project’s budget.
 Your output should be a list of overbudget projects, where each entry includes the project’s name, its budget, and the total prorated employee expenses for that project. 
 The total expenses should be rounded up to the nearest dollar. Assume all years have 365 days and disregard leap years.
+
+
+
+select a.title,a.budget,
+ceiling(datediff(a.end_date,a.start_date)*sum(c.salary)/365) as prorated_employee_expense
+from linkedin_projects a
+inner join linkedin_emp_projects b on a.id = b.project_id
+inner join linkedin_employees c on b.emp_id = c.id
+group by a.title,a.budget,a.end_date,a.start_date
+having prorated_employee_expense > a.budget
+order by a.title;
