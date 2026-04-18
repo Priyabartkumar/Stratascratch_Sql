555b10dbb758619af0d0a287bc5548efd1498c28
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-18 20:11:26 +0530

    Update Ranking_Most_Active_Guests.sql

--- Ranking_Most_Active_Guests.sql
+++ Ranking_Most_Active_Guests.sql
@@ -1,3 +1,4 @@
+Question id: 10159
 Question:
 Identify the most engaged guests by ranking them according to their overall messaging activity. The most active guest, meaning the one who has exchanged the most messages with hosts, should have the highest rank. 
 If two or more guests have the same number of messages, they should have the same rank. Importantly, the ranking shouldn't skip any numbers, even if many guests share the same rank. 
