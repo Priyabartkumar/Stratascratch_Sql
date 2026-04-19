fc9540f9a243c6ed58e9bccc6a3348fe828ded57
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-19 05:49:41 +0530

    Update Number_Of_Units_Per_Nationality.sql

--- Number_Of_Units_Per_Nationality.sql
+++ Number_Of_Units_Per_Nationality.sql
@@ -3,7 +3,7 @@ ID 10156
 Write a query that returns how many different apartment-type units (counted by distinct unit_id) are owned by people under 30, grouped by their nationality. 
 Sort the results by the number of apartments in descending order.
 
-
+Level :Medium
 queries:
 select nationality, count(distinct unit_id) as apartment_count
 from airbnb_hosts ah inner join airbnb_units au on ah.host_id = au.host_id
