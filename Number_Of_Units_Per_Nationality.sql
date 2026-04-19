15f13f8716aaf4c436372b338ad298717d65aa12
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-19 05:48:14 +0530

    Update Number_Of_Units_Per_Nationality.sql

--- Number_Of_Units_Per_Nationality.sql
+++ Number_Of_Units_Per_Nationality.sql
@@ -1,3 +1,4 @@
+ID 10156
 Write a query that returns how many different apartment-type units (counted by distinct unit_id) are owned by people under 30, grouped by their nationality. 
 Sort the results by the number of apartments in descending order.
 
