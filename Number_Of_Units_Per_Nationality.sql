52e4ae6e3f41df40eff1bbd1eb963198c4f0a578
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-19 05:47:51 +0530

    Implement query for apartment units by nationality
    Add SQL query to count distinct apartment units owned by people under 30, grouped by nationality.

--- Number_Of_Units_Per_Nationality.sql
+++ Number_Of_Units_Per_Nationality.sql
@@ -2,4 +2,9 @@ Write a query that returns how many different apartment-type units (counted by d
 Sort the results by the number of apartments in descending order.
 
 
-queries
+queries:
+select nationality, count(distinct unit_id) as apartment_count
+from airbnb_hosts ah inner join airbnb_units au on ah.host_id = au.host_id
+where au.unit_type = 'Apartment' and ah.age < 30
+group by ah.nationality
+order by apartment_count desc;
