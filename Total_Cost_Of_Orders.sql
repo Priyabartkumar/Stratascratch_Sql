6592775743709b0130e249bc9bf56df50eeb0f04
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-23 22:14:43 +0530

    Add level indicator to total cost of orders query
    Added a level indicator to the SQL query for clarity.

--- Total_Cost_Of_Orders.sql
+++ Total_Cost_Of_Orders.sql
@@ -1,6 +1,8 @@
 Q) Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. Order records by customer's first name alphabetically.
 
-Solution:
+Level: Easy
+ 
+ Solution:
  select orders.cust_id,customers.first_name, sum(total_order_cost) from orders
  join customers on  customers.id = orders.cust_id
  group by customers.id,
