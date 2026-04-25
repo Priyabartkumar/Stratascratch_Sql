5bfe6939f834b74159fa2c74fd651aee84effa42
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-25 23:50:28 +0530

    Update Average_salary.sql

--- Average_salary.sql
+++ Average_salary.sql
@@ -2,5 +2,6 @@ Question:
 Compare each employee's salary with the average salary of the corresponding department.
 Output the department, first name, and salary of employees along with the average salary of that department.
 
+Queries:
 select salary,department,first_name,
 avg(salary) over (partition by department) as avg_salary from employee;
