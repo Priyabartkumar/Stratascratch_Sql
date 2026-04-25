117a0f2875ab4f013d679827632e55e82c3f25a9
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-25 23:50:46 +0530

    Update Average_salary.sql

--- Average_salary.sql
+++ Average_salary.sql
@@ -1,3 +1,5 @@
+Level:Easy
+
 Question:
 Compare each employee's salary with the average salary of the corresponding department.
 Output the department, first name, and salary of employees along with the average salary of that department.
