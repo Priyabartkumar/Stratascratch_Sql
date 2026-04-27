c5b6d8a15c23a3ca9db8bf5ade3bbf13612e236f
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-27 23:53:00 +0530

    Update Percentage_shipable_orders.sql

--- Percentage_shipable_orders.sql
+++ Percentage_shipable_orders.sql
@@ -4,4 +4,5 @@ Find the percentage of shipable orders.
 Consider an order is shipable if the customer's address is known.
 
 queries:
-
+select count(c.address)/count(o.id)*100 as percent_shipable
+from orders o left join customers c on o.cust_id=c.id
