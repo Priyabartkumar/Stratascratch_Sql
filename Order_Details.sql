9f3c35d8baf35496eb5fe9d8044df54b4fe194d6
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-28 23:52:39 +0530

    Update Order_Details.sql

--- Order_Details.sql
+++ Order_Details.sql
@@ -1,5 +1,5 @@
 Easy     ID 9913
-Q)
+Question)
 Find order details made by Jill and Eva.
 Consider the Jill and Eva as first names of customers.
 Output the order date, details and cost along with the first name.
