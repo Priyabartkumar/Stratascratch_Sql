38b52cb83f649d87d2792d55908bc18241620189
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-06-13 23:24:30 +0530

    Add SQL query to count artist appearances in rankings

--- Artist_Appearance_Count.sql
+++ Artist_Appearance_Count.sql
@@ -4,7 +4,7 @@ Q)Find how many times each artist appeared on the Spotify ranking list.
 Output the artist name along with the corresponding number of occurrences.
 Order records by the number of occurrences in descending order
 
-Ans:
+Query:
 select artist, count(artist) as n_occurences 
 from spotify_worldwide_daily_song_ranking
 group by artist
